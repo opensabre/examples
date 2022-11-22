4b82bd8a2d40481f7cf5c17ee4d269402496c123
周涛 <zhoutaoo@foxmail.com>
2022-11-22 22:33:03 +0800

    增加简单的rest接口

---
+++ sample/src/main/java/io/github/opensabre/sample/rest/RegisteredClientController.java
@@ -0,0 +1,24 @@
+package io.github.opensabre.sample.rest;
+
+import io.github.opensabre.common.core.entity.vo.Result;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@Tag(name = "test")
+@Slf4j
+public class RegisteredClientController {
+
+
+    @Operation(summary = "测试接口", description = "hello xxx")
+    @GetMapping("/hello")
+    public Result hello(@RequestParam String name) {
+        return Result.success("Hello:" + name);
+    }
+}
