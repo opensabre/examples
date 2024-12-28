d5ca5fcd5559ee4e58934bfe3dc30588e002733b
周涛 <zhoutaoo@foxmail.com>
2024-12-28 11:23:58 +0800

    优化工程结构 by cursor ai

---
+++ sample-boot/src/main/java/com/opensabre/sample/config/ResourceServerConfig.java
@@ -0,0 +1,33 @@
+package com.opensabre.sample.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * OAuth2 Resource Server
+ */
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true, securedEnabled = true)
+public class ResourceServerConfig {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                        // 下边一行是放行接口的配置，被放行的接口上不能有权限注解，e.g. @PreAuthorize，否则无效
+                        .requestMatchers("/test02").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(resourceServerConfigurer -> resourceServerConfigurer
+                        // 可在此处添加自定义解析设置
+                        .jwt(Customizer.withDefaults())
+                );
+        return http.build();
+    }
+}
+
--- sample-boot/src/main/java/com/opensabre/sample/rest/HelloController.java
+++ sample-boot/src/main/java/com/opensabre/sample/rest/HelloController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,10 +27,12 @@ public class HelloController {
 
     @Operation(summary = "返回String", description = "String")
     @GetMapping("/echo")
+    @PreAuthorize("hasAuthority('SCOPE_read')")
     public String echo(@RequestParam("name") String name) {
         return "Hello:" + name;
     }
 
+    @PreAuthorize("hasAuthority('app')")
     @Operation(summary = "返回int", description = "int")
     @GetMapping("/hello")
     public int hello(@RequestParam("number") Integer number) {
